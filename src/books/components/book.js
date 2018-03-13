ce8c77c465e6765cc79ed7814fb3225071f8ddf4
Ricky Reusser <ricky@standardcyborg.com>
2018-03-12 17:11:13 -0700

    Books

--- src/books/components/book.js
+++ src/books/components/book.js
@@ -4,7 +4,7 @@ class Book extends React.Component {
   render () {
     return (
       <div className="book" itemProp="mainEntity" itemScope itemType="http://schema.org/Book" style={{marginBottom:"1.0em", marginLeft:"4.6em", textIndent:"-4.6em"}}>
-        <em itemProp="dateCreated" style={{marginRight:"1em"}}>{this.props.dateRead}</em><a href={this.props.url} itemProp="name">{this.props.title}</a>, <span itemProp="author">{this.props.author}</span>
+        <em itemProp="dateCreated" style={{marginRight:"1em"}}>{this.props.dateRead}</em><a href={this.props.url} target="_blank" itemProp="name">{this.props.title}</a>, <span itemProp="author">{this.props.author}</span>
       </div>
     );
   }
