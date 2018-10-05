86b93e95681b1f1a6d7f233a10c134a02b78d349
Ricky Reusser <ricky@standardcyborg.com>
2018-10-05 00:04:35 -0700

    🐴

--- src/src/magnet/index.js
+++ src/src/magnet/index.js
@@ -33,16 +33,11 @@ require('regl')({
 function run (regl) {
   var state = {
     dirty: true,
-    alpha: 1.2,
-    steps: 8,
-    width: 2.0,
-    noiseScale: 0.8,
-    noiseSpeed: 0.4,
-    resolution: 128,
-    modulation: 1.0,
-    modulationFrequency: 0.4,
-    modulationSpeed: 6.0,
-    cylinderRadius: 1.0,
+    alpha: 1.0,
+    steps: 4,
+    width: 2.8,
+    strength: 1.0,
+    resolution: 96
   };
 
   var screenWidth, screenHeight, h, w, licRadius, dt, alpha;
@@ -54,8 +49,8 @@ function run (regl) {
     h = state.resolution;
     w = Math.floor(h * screenWidth / screenHeight);;
     licRadius = 0.35;
-    dt = licRadius / state.steps * 0.3;
-    alpha = 0.5 / w / state.steps / licRadius / state.width * screenWidth;
+    dt = licRadius / state.steps * 0.2;
+    alpha = 0.6 / w / state.steps / licRadius / state.width * screenWidth;
   }
 
   var states = [];
@@ -83,6 +78,7 @@ function run (regl) {
   computeConstants();
   resize();
   window.addEventListener('resize', function () {
+    state.dirty = true;
     computeConstants();
     resize();
   });
@@ -91,18 +87,14 @@ function run (regl) {
   document.body.appendChild(createControls(null, controlRoot));
 
   require('control-panel')([
-    {label: 'alpha', type: 'range', min: 0, max: 1, initial: state.alpha, step: 0.01},
+    {label: 'alpha', type: 'range', min: 0, max: 2, initial: state.alpha, step: 0.01},
     {label: 'steps', type: 'range', min: 2, max: 20, initial: state.steps, step: 1},
-    {label: 'noiseScale', type: 'range', min: 0.1, max: 2, initial: state.noiseScale, step: 0.1},
-    {label: 'noiseSpeed', type: 'range', min: 0.1, max: 2, initial: state.noiseSpeed, step: 0.1},
     {label: 'resolution', type: 'range', min: 8, max: 512, initial: state.resolution, step: 1},
     {label: 'width', type: 'range', min: 0.5, max: 4, initial: state.width, step: 0.1},
-    {label: 'modulation', type: 'range', min: 0.0, max: 1, initial: state.modulation, step: 0.01},
-    {label: 'modulationFrequency', type: 'range', min: 0.1, max: 4, initial: state.modulationFrequency, step: 0.1},
-    {label: 'modulationSpeed', type: 'range', min: 0.1, max: 4, initial: state.modulationSpeed, step: 0.1},
   ], {
     root: controlRoot
   }).on('input', data => {
+    state.dirty = true;
     var needsResize = data.resolution !== state.resolution;
     Object.assign(state, data)
     computeConstants();
@@ -111,7 +103,7 @@ function run (regl) {
 
   window.addEventListener('mousewheel', function (ev) {
     ev.preventDefault();
-    state.cylinderRadius *= Math.exp(-ev.deltaY * 0.01);
+    state.strength *= Math.exp(-ev.deltaY * 0.01);
     state.dirty = true;
   });
 
@@ -155,73 +147,33 @@ function run (regl) {
     frag: glsl`
       precision highp float;
 
-      #pragma glslify: snoise = require(glsl-noise/simplex/3d)
-
       varying vec2 uv;
       uniform sampler2D src;
       uniform float uDt;
-      uniform float uAspect, uNoiseScale, uNoiseSpeed, uTime, uCylinderRadius;
+      uniform float uAspect, uNoiseScale, uNoiseSpeed, uTime, uCylinderRadius, uStrength;
       uniform vec2 uCenter;
 
-      vec2 curlNoise(vec3 p) {
-        float eps = 0.01;
-        float fp, fm, a, b;
-        fp = snoise(vec3(p.xy + vec2(0.0, eps), p.z));
-        fm = snoise(vec3(p.xy + vec2(0.0, -eps), p.z));
-        a = (fp - fm);
-        fp = snoise(vec3(p.xy + vec2(eps, 0.0), p.z));
-        fm = snoise(vec3(p.xy + vec2(-eps, 0.0), p.z));
-        b = (fp - fm);
-        return vec2(a, -b) * 0.5 / eps;
-      }
-
       vec2 dfdt (vec2 f) {
-        float x0 = f.x;
-        float y0 = f.y;
-        f -= uCenter;
+        vec2 p1 = vec2(0.0, -0.5);
+        vec2 p2 = vec2(0.0, 0.5);
 
-        float cylinderRadius2 = uCylinderRadius * uCylinderRadius;
+        vec2 dx1 = f - p1;
+        vec2 dx2 = f - p2;
+        float r2_1 = dot(dx1, dx1);
+        float r2_2 = dot(dx2, dx2);
 
-        float r2 = dot(f, f);
-
-        vec2 flow = vec2(1.0, 0.0);
-
-        vec2 doublet = vec2(
-          (f.y * f.y - f.x * f.x),
-          -2.0 * f.x * f.y
-        ) * cylinderRadius2 / (r2 * r2);
-
-        vec2 vortex = vec2(
-          f.y,
-          -f.x
-        ) / r2;
-
-        vec2 turbulence = curlNoise(vec3(
-          (x0 - uTime * 0.6) * uNoiseScale,
-          y0 * uNoiseScale,
-          uTime * 0.40 * uNoiseSpeed
-        ));
-
-        turbulence *= smoothstep(r2, cylinderRadius2, cylinderRadius2 * 1.4);
-        float yoffset = f.y + f.x * uCenter.y * 0.1;
-
-        turbulence *= 
-          (f.x > 0.0 ? 1.0 : 0.0) *
-          smoothstep(0.5 + sqrt(abs(f.x * 48.0)), 0.5, yoffset * yoffset / cylinderRadius2) *
-          exp(-f.x * f.x / 25.0);
-
-        vec2 v = (
-          1.0 * flow +
-          1.0 * doublet +
-          0.5 * uCenter.y * vortex +
-          1.25 * uCylinderRadius * turbulence
+        // A dipole
+        float dy = 0.01;
+        vec2 dx3 = f - uCenter + vec2(0.0, dy).yx;
+        float r2_3 = dot(dx3, dx3);
+        vec2 dx4 = f - uCenter - vec2(0.0, dy).yx;
+        float r2_4 = dot(dx4, dx4);
+        
+        return normalize(
+          dx1 / r2_1 +
+          -dx2 / r2_2 +
+          (dx3 / r2_3 - dx4 / r2_4) / dy * uStrength
         );
-        v *= (r2 > cylinderRadius2 ? 1.0 : 0.0);
-
-        //float vmag = length(v);
-        //v = v / vmag;
-
-        return v;
       }
 
       vec4 deriv4 (vec4 f) {
@@ -247,14 +199,12 @@ function run (regl) {
     attributes: {xy: [-4, -4, 0, 4, 4, -4]},
     uniforms: {
       uTime: ctx => ctx.time,
-      uNoiseScale: () => 1.0 / (state.noiseScale * state.cylinderRadius),
       uAspect: () => screenWidth / screenHeight,
       src: regl.prop('src'),
       uResolution: ctx => [1 / ctx.framebufferWidth, 1 / ctx.framebufferHeight],
-      uNoiseSpeed: () => state.noiseSpeed,
       uDt: dt,
       uCenter: () => state.center,
-      uCylinderRadius: () => state.cylinderRadius * 0.3,
+      uStrength: () => state.strength,
     },
     framebuffer: regl.prop('dst'),
     depth: {enable: false},
@@ -279,7 +229,10 @@ function run (regl) {
       uniform sampler2D src;
       varying vec2 uv;
       void main () {
-        gl_FragColor = (uv + uResolution * vec2(random(gl_FragCoord.xy), random(gl_FragCoord.xy + 0.5))).xyxy;
+        gl_FragColor = (uv + uResolution * vec2(
+          random(gl_FragCoord.xy),
+          random(gl_FragCoord.xy + 0.5)
+        )).xyxy;
       }
     `,
     attributes: {xy: [-4, -4, 0, 4, 4, -4]},
@@ -297,7 +250,7 @@ function run (regl) {
     vert: `
       precision highp float;
       uniform mat4 uProjectionView;
-      uniform float uLineWidth, uAspect, uDir, uPhase;
+      uniform float uLineWidth, uAspect, uDir;
       uniform sampler2D uState1, uState2;
       uniform vec2 uIntensity, uX;
       attribute vec2 aLine;
@@ -313,30 +266,31 @@ function run (regl) {
         gl_Position = vec4(mix(p, n, aLine.y) * 2.0 - 1.0, 0, 1);
         gl_Position.xy += normalize((p.yx - n.yx) * vec2(1, uAspect)) * vec2(-1.0 / uAspect, 1) * aLine.x * uLineWidth * (0.5 + 1.0 * vAlpha);
         vLineX = aLine.x;
-        vX = mix(uX.x, uX.y, mix(1.0 - aLine.y, aLine.y, uDir)) * (uDir * 2.0 - 1.0) - uPhase + aLUT.w;
+        vX = mix(uX.x, uX.y, mix(1.0 - aLine.y, aLine.y, uDir)) * (uDir * 2.0 - 1.0) + aLUT.w;
       }
     `,
     frag: `
       precision highp float;
       varying float vAlpha, vLineX, vX;
-      uniform float uAlpha, uFeather, uModulationFreq, uModulation;
+      uniform float uAlpha, uFeather;
       #define PI 3.14159265
+
+      highp float random(vec2 co) {
+        highp float a = 12.9898;
+        highp float b = 78.233;
+        highp float c = 43758.5453;
+        highp float dt= dot(co.xy ,vec2(a,b));
+        highp float sn= mod(dt,3.14);
+        return fract(sin(sn) * c);
+      }
+
       void main () {
-        float modulation = 2.0 * mod(vX * uModulationFreq, PI) / PI - 1.0;
-        modulation *= modulation;
-        modulation = 1.0 - modulation;
-        modulation *= modulation;
-        modulation = mix(1.0, modulation, uModulation);
+        float r = random(gl_FragCoord.xy);
 
-        gl_FragColor = vec4(vec3(1), uAlpha * vAlpha * modulation * (
+        gl_FragColor = vec4(vec3(1), uAlpha * vAlpha * (
           smoothstep(1.0, 1.0 - uFeather, vLineX) *
           smoothstep(-1.0, -1.0 + uFeather, vLineX)
-        ));
-
-        /*gl_FragColor = vec4(vec3(1), uAlpha * vAlpha * (
-          smoothstep(1.0, 1.0 - uFeather, vLineX) *
-          smoothstep(-1.0, -1.0 + uFeather, vLineX)
-        ));*/
+        ) * r);
       }
     `,
     attributes: {
@@ -355,9 +309,6 @@ function run (regl) {
     },
     depth: {enable: false},
     uniforms: {
-      uModulation: () => state.modulation,
-      uModulationFreq: () => state.modulationFrequency,
-      uPhase: ctx => (-ctx.time * state.modulationSpeed * Math.PI * 2) % (Math.PI * 2.0 / state.modulationFrequency),
       uX: regl.prop('x'),
       uAlpha: () => state.alpha * alpha,
       uDir: regl.prop('dir'),
@@ -389,8 +340,7 @@ function run (regl) {
       varying vec2 uv;
       uniform sampler2D src;
       void main () {
-        //gl_FragColor = vec4(vec3(texture2D(src, uv)), 1.0);
-        gl_FragColor = colormap((texture2D(src, uv).x));
+        gl_FragColor = colormap(pow(1.0 - texture2D(src, uv).x, 2.0));
       }
     `,
     attributes: {xy: [-4, -4, 0, 4, 4, -4]},
@@ -404,6 +354,7 @@ function run (regl) {
   }
   
   regl.frame(({tick}) => {
+    if (!state.dirty) return;
     //if (tick % 3 !== 1) return;
     //if (!state.dirty) return;
     licAccumulatorFbo.use(() => regl.clear({color: [0, 0, 0, 1]}));
