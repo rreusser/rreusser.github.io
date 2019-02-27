2b699247dd5eb6944e61785335200e7139487932
Ricky Reusser <rsreusser@gmail.com>
2019-02-26 20:38:07 -0800

    🔪

--- src/lib/default-idyll-components/header.js
+++ src/lib/default-idyll-components/header.js
@@ -3,44 +3,14 @@ import classNames from 'classnames';
 import resl from 'resl';
 
 class Header extends React.PureComponent {
-	constructor (props) {
-    super(props);
-
-    this.state = {isLoaded: false};
-  }
-
-  componentDidMount () {
-    if (this.props.bgImageSrc) {
-      resl({
-        manifest: {
-          bg: {
-            src: this.props.bgImageSrc,
-            type: 'image',
-          }
-        },
-        onDone: () => {
-          this.setState({isLoaded: true});
-        }
-      });
-    }
-  }
-
   render () {
-    if (this.props.bgImagePreviewSrc) {
-      var previewStyleProps = {};
-      previewStyleProps.backgroundImage = 'url('+this.props.bgImagePreviewSrc+')';
-    }
     if (this.props.bgImageSrc) {
       var styleProps = {};
       styleProps.backgroundImage = 'url('+this.props.bgImageSrc+')';
     }
     return (
-      <div className={classNames('article-header', {
-          'article-header--has-image': !!this.props.bgImageSrc,
-          'article-header--has-image-preview': !!this.props.bgImagePreviewSrc
-        })}>
-        {(this.props.bgImageSrc && this.state.isLoaded) && (<div className="article-header__image" style={styleProps}/>)}
-        {this.props.bgImagePreviewSrc && (<div className={classNames("article-header__image-preview", {"article-header__image-preview--hidden": !!this.state.isLoaded})} style={previewStyleProps}/>)}
+      <div className={classNames('article-header')}>
+        {this.props.bgImageSrc && (<div className="article-header__image" style={styleProps}/>)}
         <div className={'article-header__content'}>
           <h1 className={'hed'}>
             {this.props.title}
