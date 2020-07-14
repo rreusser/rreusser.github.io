64abe8fe4742dff5a90902be0868d359a3d0e7d3
Ricky Reusser <ricky@standardcyborg.com>
2020-07-14 07:54:15 -0700

    🐻

--- src/lib/default-idyll-components/menu.js
+++ src/lib/default-idyll-components/menu.js
@@ -16,23 +16,10 @@ class Menu extends React.Component {
 
   render () {
     return (
-      <nav className={classNames("menu", {"menu--expanded": this.state.expanded})}>
-        <button className="menu__thumb" onClick={this.handleClick}>
-          <span className="menu__hamburger"></span>
-          <span className="menu__hamburger"></span>
-          <span className="menu__hamburger"></span>
-        </button>
-        <div className="menu__content">
-          <div className="menu__heading">
-            <a href="/">rreusser.github.io</a>
-          </div>
-          <div className="menu__items">
-            <a className="menu__item" href="/sketches/">Sketches</a>
-            <a className="menu__item" href="/projects/">Projects</a>
-            <a className="menu__item" href="https://github.com/rreusser">github.com/rreusser</a>
-            <a className="menu__item" href="https://twitter.com/rickyreusser">@rickyreusser</a>
-          </div>
-        </div>
+      <nav className="menu">
+        <a className="menu__item" href="/">rreusser.github.io</a>
+        <a className="menu__item" href="/sketches/">sketches</a>
+        <a className="menu__item" href="/projects/">projects</a>
       </nav>
     );
   }
